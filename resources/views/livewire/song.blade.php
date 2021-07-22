d256acb5131d6be09cfa94993ca24b60b447ded7
Magic <cardhousemagic@gmail.com>
2021-07-21 22:09:05 -0400

    Added icons and smaller view for song component

--- resources/views/livewire/song.blade.php
+++ resources/views/livewire/song.blade.php
@@ -1,18 +1,21 @@
 <div x-data="{ open: @entangle('active')}" x-show="open" class="p-3 bg-white hover:bg-gray-300 cursor-pointer">
     <div >
-        <div class="flex items-middle justify-between">
-            <h1 class="font-bold">{{ $track->title }}</h1>
-            <button type="button" wire:click="trash({{ $track }})">Delete</button>
+        <div class="flex items-middle justify-between align-middle">
+            <h1 class="font-bold text-xl mb-3 w-1/2">
+                {{ $track->title }}
+                <span class="text-sm text-gray-500">({{ $track->platform }})</span>
+            </h1>
+            <div class="flex items-center space-x-2 w-1/6">
+                <img src="https://www.svgrepo.com/show/21106/thumbs-up.svg" intrinsicsize="512 x 512" width="25" height="25" srcset="https://www.svgrepo.com/show/21106/thumbs-up.svg 4x" alt="Thumbs Up SVG Vector" title="Thumbs Up SVG Vector">
+                <span>{{ $track->likes }}</span>
+            </div>
+            <div class="flex items-center space-x-2 w-1/6">
+                <img src="https://www.svgrepo.com/show/313747/comment.svg" intrinsicsize="512 x 512" width="25" height="25" srcset="https://www.svgrepo.com/show/313747/comment.svg 4x" alt="Comment SVG Vector" title="Comment SVG Vector">
+                <span>{{ $track->notes->count() }}</span>
+            </div>
+            <button type="button" wire:click="trash({{ $track }})">
+                <img src="https://www.svgrepo.com/show/2143/trash.svg" intrinsicsize="512 x 512" width="25" height="25" srcset="https://www.svgrepo.com/show/2143/trash.svg 4x" alt="Trash SVG Vector" title="Trash SVG Vector"></button>
         </div>
-        {{ $track->url }}<span>on {{ $track->platform }}</span>
-        <br>
-        <p>Liked {{ $track->likes }} out of {{ $track->total_plays }} plays</p>
-        @forelse ($track->notes as $note)
-            <ul>
-                <li>{{ $note->user->name }}: {{ $note->body }}</li>
-            </ul>
-        @empty
-            No feedback
-        @endforelse
+       
     </div>
 </div>
